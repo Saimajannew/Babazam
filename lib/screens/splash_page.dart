808bb7f586f4adbc5226c1188ac2ebbdca97ee12
Saima <eng.saimarahimi@gmail.com>
2023-04-29 13:37:05 +0430

    initial commit

---
+++ lib/screens/splash_page.dart
@@ -0,0 +1,78 @@
+import 'package:ecommerce_int2/app_properties.dart';
+import 'package:ecommerce_int2/screens/auth/welcome_back_page.dart';
+import 'package:flutter/material.dart';
+
+import 'intro_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> opacity;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+        duration: Duration(milliseconds: 4000), vsync: this);
+    opacity = Tween<double>(begin: 1.0, end: 0.0).animate(controller)
+      ..addListener(() {
+        setState(() {});
+      });
+    controller.forward().then((_) {
+      navigationPage();
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  void navigationPage() {
+    Navigator.of(context)
+        .pushReplacement(MaterialPageRoute(builder: (_) => IntroPage()));
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/background.png'), fit: BoxFit.cover)),
+      child: Container(
+        decoration: BoxDecoration(color: Colors.yellow),
+        child: SafeArea(
+          child: new Scaffold(
+            body: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Opacity(
+                      opacity: opacity.value,
+                      child: new Image.asset('assets/babazam.png')),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RichText(
+                    text: TextSpan(
+                        style: TextStyle(color: Colors.black),
+                        children: [
+                          TextSpan(text: ''),
+                          TextSpan(
+                              text: '',
+                              style: TextStyle(fontWeight: FontWeight.bold))
+                        ]),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
